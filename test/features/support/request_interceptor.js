e014d5445ad6a438bcf3ec21e6afc14b8c45a42b
Andrés Correa Casablanca <andreu@adsmurai.com>
2018-01-09 18:16:49 +0100

    Prototype cucumber step definition (http request intercept)

--- test/features/support/request_interceptor.js
+++ test/features/support/request_interceptor.js
@@ -1,5 +1,4 @@
 const { Then } = require('cucumber');
-const { expect } = require('chai');
 
 Then('the browser sends a {string} request to {string}', function(httpVerb, url, callback) {
     callback('Not implemented!');
