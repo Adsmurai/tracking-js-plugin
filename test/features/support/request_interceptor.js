06d9e784e85559a9138a5bd6c4d7808717ba7dd2
Toni Penya-Alba <toni.penya@adsmurai.com>
2018-01-11 09:33:42 +0100

    capture ajax request fulfilment in test.

--- test/features/support/request_interceptor.js
+++ test/features/support/request_interceptor.js
@@ -4,7 +4,17 @@ Then('the browser sends a {string} request to {string}', function(httpVerb, url,
     const STATUS_CODE = 200;    // TODO: Get rid of this magick number.
 
     browser.expectRequest(httpVerb, url, STATUS_CODE);
-    browser.assertRequests();
+    err = browser.assertRequests();
 
-    callback(null, "All is fine!");
+    // TODO: Check which standard of promises is being used (https://promisesaplus.com/)
+    // TODO: specify the success and err methods in separate calls to the promise
+    err.then(
+        function(value) {
+            console.log('success: ', value);
+            callback();
+        },
+        function(reason) {
+            callback(reason, "Error with ajax request.");
+        },
+    );
 });
