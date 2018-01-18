d4647372be2d52ddee3cd726757af623d7c8942e
Toni Penya-Alba <toni.penya@adsmurai.com>
2018-01-18 16:51:26 +0100

    Unit-test uuidV4 generation

---
+++ test/specs/test-uuidv4.js
@@ -0,0 +1,61 @@
+const assert = require('assert');
+const webdriverio = require('webdriverio');
+const options = {
+    logLevel: 'silent',
+    desiredCapabilities: {
+        browserName: 'chrome',
+    },
+    host: 'tracking-selenium-hub.sandbox',
+    port: 4444,
+};
+
+describe('uuidv4', function () {
+    const browser = webdriverio.remote(options);
+
+    before('init browser session', function (done) {
+        browser
+            .init()
+            .url('https://tracking-test.adsmurai.local')
+            .then(() => done());
+    });
+
+    it('returns different uuids in different calls', function (done) {
+        let firstUuidGenerated;
+        browser
+            .execute(function () {
+                return window.adsmurai_tracking.utils.uuidv4();
+            })
+            .then(function (value) {
+                firstUuidGenerated = value.value;
+                return browser.execute(function () {
+                    return window.adsmurai_tracking.utils.uuidv4();
+                });
+            })
+            .then(function (value) {
+                assert.notEqual(value.value, firstUuidGenerated);
+                done();
+            })
+            .catch(done);
+    });
+
+    it('the uuid returned is compliant with the specification', function (done) {
+        browser
+            .execute(function () {
+                return window.adsmurai_tracking.utils.uuidv4();
+            })
+            .then(function (value) {
+                const generatedUuid = value.value;
+                const uuidv4Regex = /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
+                assert.ok(uuidv4Regex.test(generatedUuid), 'offending uuid: ' + generatedUuid);
+                done();
+            })
+            .catch(done);
+    });
+
+    after('finish browser session', function (done) {
+        browser
+            .end()
+            .then(() => done())
+            .catch(done);
+    });
+});
