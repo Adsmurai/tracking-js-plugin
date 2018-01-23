c180433229c9f5e0cd0eb61a76cf6d5716d3f3ef
Toni Penya-Alba <toni.penya@adsmurai.com>
2018-01-23 12:09:25 +0100

    Load tracking code asynchronously from script tag

--- src/adsmurai-tracking.js
+++ src/adsmurai-tracking.js
@@ -1,96 +1,99 @@
 'use strict';
+(function(name, definition){
+    window[name] = definition();
+})('AdsmuraiTracking', function(){
+    const TRACKING_API_DOMAIN = window.adsmurai_tracking.TRACKING_API_DOMAIN;
 
-const AdsmuraiTracking = function() {
-    this.pageViewId = this.utils.uuidv4();
-    this.fingerprint = {
-        hash: null,
-        components: null
-    };
+    const AdsmuraiTracking = function() {
+        this.pageViewId = this.utils.uuidv4();
+        this.fingerprint = {
+            hash: null,
+            components: null
+        };
 
-    const _adsmuraiTracking = this;
-    loadFingerprintingJavascript()
-        .then(calculateFingerprint)
-        .then(injectTracking);
+        const _adsmuraiTracking = this;
+        loadFingerprintingJavascript()
+            .then(calculateFingerprint)
+            .then(injectTracking);
 
-    function loadFingerprintingJavascript() {
-        if (typeof(Fingerprint2) !== 'undefined') {
-            return new Promise.resolve();
+        function loadFingerprintingJavascript() {
+            if (typeof(Fingerprint2) !== 'undefined') {
+                return new Promise.resolve();
+            }
+
+            const fingerprintjs2Element = document.createElement('script');
+
+            return new Promise(function(resolve) {
+                fingerprintjs2Element.onload = function() {
+                    resolve();
+                };
+
+                fingerprintjs2Element.src = 'https://cdnjs.cloudflare.com/ajax/libs/fingerprintjs2/1.5.1/fingerprint2.min.js';
+                document.head.appendChild(fingerprintjs2Element);
+            });
         }
 
-        const fingerprintjs2Element = document.createElement('script');
-
-        return new Promise(function(resolve) {
-            fingerprintjs2Element.onload = function() {
-                resolve();
-            };
-
-            fingerprintjs2Element.src = 'https://cdnjs.cloudflare.com/ajax/libs/fingerprintjs2/1.5.1/fingerprint2.min.js';
-            document.head.appendChild(fingerprintjs2Element);
-        });
-    }
-
-    function calculateFingerprint() {
-        return new Promise(function(resolve) {
-            new Fingerprint2().get(function(fingerprintHash, fingerprintComponents){
-                resolve({
-                    hash: fingerprintHash,
-                    components: fingerprintComponents
+        function calculateFingerprint() {
+            return new Promise(function(resolve) {
+                new Fingerprint2().get(function(fingerprintHash, fingerprintComponents){
+                    resolve({
+                        hash: fingerprintHash,
+                        components: fingerprintComponents
+                    });
                 });
+
             });
+        }
 
+        function injectTracking(fingerprint) {
+            _adsmuraiTracking.fingerprint.hash = fingerprint.hash;
+            _adsmuraiTracking.fingerprint.components = fingerprint.components;
+        }
+    };
+
+    AdsmuraiTracking.prototype.registerEvent = function(eventName, eventData) {
+        if (this.utils.isDoNotTrackEnabled()) return;
+
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', 'https://' + TRACKING_API_DOMAIN + '/' + eventName);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify(eventData));
+    };
+
+    AdsmuraiTracking.prototype.registerPageViewEvent = function() {
+        // TODO: this method should return a promise that's resolved after the servers responds
+        this.registerEvent('pageView', {
+            pageViewId: this.pageViewId,
+            url: window.location.href,
+            referrer: document.referrer,
+            fingerprint: this.fingerprint
         });
-    }
+    };
 
-    function injectTracking(fingerprint) {
-        _adsmuraiTracking.fingerprint.hash = fingerprint.hash;
-        _adsmuraiTracking.fingerprint.components = fingerprint.components;
-    }
-};
+    AdsmuraiTracking.prototype.utils = {
+        uuidv4: function() {
+            /* Following  RFC4122 version 4 UUID. Implementation from https://stackoverflow.com/a/2117523 */
+            const randomValues = new Uint32Array(32);
+            const randomIterator = window
+                .crypto.getRandomValues(randomValues)
+                .map(x => x%16)
+                .values();
 
-AdsmuraiTracking.prototype.registerEvent = function(eventName, eventData) {
-    if (this.utils.isDoNotTrackEnabled()) return;
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                const r = randomIterator.next().value;
+                const v = c === 'x' ? r : (r & 0x3 | 0x8);
+                return v.toString(16);
+            });
+        },
+        isDoNotTrackEnabled: function() {
+            return (
+                !!(navigator.doNotTrack-0)     || // Current & standard check
+                !!(window.doNotTrack-0)        || // MSIE 11 & MS Edge & Safari 7.1.3+
+                !!(navigator.msDoNotTrack-0)   || // MSIE 9 & MSIE 10
+                'yes' === navigator.doNotTrack    // Firefox < v32.0
+            );
+        }
+    };
 
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://' + window.adsmurai_tracking.TRACKING_API_DOMAIN + '/' + eventName);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify(eventData));
-};
-
-AdsmuraiTracking.prototype.registerPageViewEvent = function() {
-    // TODO: this method should return a promise that's resolved after the servers responds
-    this.registerEvent('pageView', {
-        pageViewId: this.pageViewId,
-        url: window.location.href,
-        referrer: document.referrer,
-        fingerprint: this.fingerprint
-    });
-};
-
-AdsmuraiTracking.prototype.utils = {
-    uuidv4: function() {
-        /* Following  RFC4122 version 4 UUID. Implementation from https://stackoverflow.com/a/2117523 */
-        const randomValues = new Uint32Array(32);
-        const randomIterator = window
-            .crypto.getRandomValues(randomValues)
-            .map(x => x%16)
-            .values();
-
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-            const r = randomIterator.next().value;
-            const v = c === 'x' ? r : (r & 0x3 | 0x8);
-            return v.toString(16);
-        });
-    },
-    isDoNotTrackEnabled: function() {
-        return (
-            !!(navigator.doNotTrack-0)     || // Current & standard check
-            !!(window.doNotTrack-0)        || // MSIE 11 & MS Edge & Safari 7.1.3+
-            !!(navigator.msDoNotTrack-0)   || // MSIE 9 & MSIE 10
-            'yes' === navigator.doNotTrack    // Firefox < v32.0
-        );
-    }
-};
-
-(function(_window) {
-    _window.adsmurai_tracking = Object.assign(new AdsmuraiTracking(), _window.adsmurai_tracking);
-})(window);
+    return AdsmuraiTracking;
+});
