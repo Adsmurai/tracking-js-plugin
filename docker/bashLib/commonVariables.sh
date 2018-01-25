b0060ef58fc3757a19fb97cea33797b80a16e28e
Andrés Correa Casablanca <andreu@adsmurai.com>
2018-01-25 09:02:28 +0100

    Define environment variables conditionally respect the CI environment

--- docker/bashLib/commonVariables.sh
+++ docker/bashLib/commonVariables.sh
@@ -10,13 +10,19 @@ HOST_USER_NAME="$(whoami)"
 export HOST_USER_ID="$(id -u "${HOST_USER_NAME}")"
 export HOST_GROUP_ID="$(id -g "${HOST_USER_NAME}")"
 
-TERMINAL_MODE=${TERMINAL_MODE:-it}
-DEBUG_OUTPUT_FILE=${DEBUG_OUTPUT_FILE:-/dev/null}
-
-RANDOM_SUFFIX="$(openssl rand -base64 64 | tr -dc 'a-z0-9_' | fold -w 32 | head -n 1;)"
+if [[ -n "${CI_PIPELINE_ID}" ]]; then
+    # We detect that the script is running inside a CI pipeline, so we become verbose and non-interactive
+    DEBUG_OUTPUT_FILE=${DEBUG_OUTPUT_FILE:-/dev/stdout}
+    TERMINAL_MODE="t"
+else
+    DEBUG_OUTPUT_FILE=${DEBUG_OUTPUT_FILE:-/dev/null}
+    TERMINAL_MODE=${TERMINAL_MODE:-it}
+fi
 
 if [ ${TERMINAL_MODE} = "t" ]; then
     DOCKER_RUN_TERMINAL_MODE="-T"
 else
     DOCKER_RUN_TERMINAL_MODE=""
 fi
+
+RANDOM_SUFFIX="$(openssl rand -base64 64 | tr -dc 'a-z0-9_' | fold -w 32 | head -n 1;)"
